2d77737a5c7235b32247c4e4769d3a2c1404d119
codeeverywhereca <codeeverywhereca@users.noreply.github.com>
2016-12-05 22:18:37 -0500

    Update angular-unique.js

--- unique/angular-unique.js
+++ unique/angular-unique.js
@@ -9,9 +9,10 @@ app.filter('unique', function() {
 	return function(arr, attr) {
 		if( arr === undefined || arr.length == 0 ) return [];
 		var temp = [];
-		for( var x=0; x<arr.length; x++ ) {
-			if( temp.indexOf(arr[x][attr]) == -1 ) {
-				temp.push(arr[x][attr]);
+		for( var x=0; x<arr.length; x++ ) {			
+			var val = ( attr === undefined ) ? arr[x] : arr[x][attr];
+			if( temp.indexOf( val ) == -1 ) {
+				temp.push( val );
 			}
 		}
 		return temp;
